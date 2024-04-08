2598442a9710a580c1eedbdeac28abbc8b93ca75
dir-FY24kageyamanaoki <naoki.kageyama.c382@dirbato.co.jp>
2024-04-08 12:06:03 +0900

    レッスン2の総括コミット

---
+++ Sample/src/sample/ForSample.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class ForSample {
+
+    public static void main(String[] args) {
+        
+        for ( var i = 3 ; i <= 18 ; i++) {
+           
+            System.out.println(i);
+            
+    }
+
+}
+}
